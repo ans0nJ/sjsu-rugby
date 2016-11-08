4557125573e5637cfed89eb80d1144ab84a43239
ans0nJ <ans0nJ72@gmail.com>
2016-11-08 14:24:53 -0800

    Updated Schedule

---
+++ SQL/schedule.sql
@@ -0,0 +1,17 @@
+/*
+INSERT INTO schedule VALUES ("Oct 1st", "7s Tournament @ San Diego State University", "San Diego, CA", "10:00AM", "");
+INSERT INTO schedule VALUES ("Nov 5th/6th", "SlugFest 15s Tournament @ UC Santa Cruz", "Santa Cruz, CA", "10:00AM", "");
+INSERT INTO schedule VALUES ("Nov 12th", "UC Santa Cruz HOME GAME", "San Jose, CA", "1:00PM", "");
+INSERT INTO schedule VALUES ("Nov 19th", "Sonoma State University AWAY GAME", "Rohnert Park, CA", "1:00PM", "");
+*/
+INSERT INTO schedule VALUES ("Dec 10th", "Pac West 7s Championship", "TBA", "TBA", "");
+INSERT INTO schedule VALUES ("Jan 28th", "University of Nevada, Reno HOME GAME", "San Jose, CA", "1:00PM", "");
+INSERT INTO schedule VALUES ("Feb 4th", "UC Davis AWAY GAME", "Davis, CA", "1:00PM", "");
+INSERT INTO schedule VALUES ("Feb 11th", "UC Santa Cruz HOME GAME", "San Jose, CA", "1:00PM", "");
+INSERT INTO schedule VALUES ("Feb 18th", "Chico State University HOME GAME", "San Jose, CA", "1:00PM", "");
+INSERT INTO schedule VALUES ("Feb 25th", "BYE", "", "", "");
+INSERT INTO schedule VALUES ("Mar 4th", "Stanford University AWAY GAME", "Stanford, CA", "1:00PM", "");
+INSERT INTO schedule VALUES ("Mar 11th", "San Francisco State University HOME GAME", "San Jose, CA", "1:00PM", "");
+INSERT INTO schedule VALUES ("Mar 18th", "Fresno State University AWAY GAME", "Fresno, CA", "1:00PM", "");
+INSERT INTO schedule VALUES ("Mar 25th", "BYE", "", "", "");
+INSERT INTO schedule VALUES ("Apr 1st/2nd", "Pac West 15s Tournament", "TBA", "TBA", "");
--- schedule.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO schedule VALUES ("Oct 1st", "7s Tournament @ San Diego State University", "San Diego, CA", "10:00AM", "");
-INSERT INTO schedule VALUES ("Nov 5th/6th", "SlugFest 15s Tournament @ UC Santa Cruz", "Santa Cruz, CA", "10:00AM", "");
-INSERT INTO schedule VALUES ("Nov 12th", "UC Santa Cruz HOME GAME", "San Jose, CA", "1:00PM", "");
-INSERT INTO schedule VALUES ("Nov 19th", "Sonoma State University AWAY GAME", "Rohnert Park, CA", "1:00PM", "");
